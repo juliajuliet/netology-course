ff6c43763ddf3e4ca6456852d3b29ef7d11fbe48
juliajuliet <38317737+juliajuliet@users.noreply.github.com>
2022-05-26 22:41:20 +0700

    Hometask 2

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="app">
+
+        <app-header/>
+
+        <div class="container">
+            <h1 class="pt-3 pb-3">Персонажи Marvel</h1>
+            
+            <app-modal :character="characters[characterIndex]"/>
+
+            <spinner/>
+
+            <div class="row">
+                <div v-for ="(character,idx) in characters"
+                :key="character.id"
+                class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img :src="character.thumbnail" class="img-fluid rounded-start" :alt="character.name">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">{{character.name}}</h5>
+                                <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                @click="characterIndex=idx">
+                                    Подробнее
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Spinner from "./components/Spinner";
+    import AppModal from "./components/AppModal";
+    import AppHeader from "./components/AppHeader";
+
+    export default {
+        name: 'App',
+        components: {
+            AppHeader,
+            AppModal,
+            Spinner,
+        },
+        data() {
+            return {
+                loading: false,
+                characters: [],
+                characterIndex: 0,
+            }
+        },
+        methods: {
+            fetchCharacters: function() {
+                fetch('https://netology-api-marvel.herokuapp.com/characters')
+                .then(res => res.json())
+                .then(json => this.characters = json)
+            }
+        },
+        computed: {},
+        mounted() {
+            this.fetchCharacters();
+        }
+    }
+</script>
+
+<style>
+
+</style>
